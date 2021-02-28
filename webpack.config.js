4beaafa285c5deac6399bdbcd2f33e4988dbd3d0
Onton <bashprog@gmail.com>
2021-02-28 22:37:34 +0600

    add configs

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, `src/index.js`)
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
+        // publicPath: '/',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        // host: '192.168.1.228',
+        port: 3001,
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, 'src/components'),
+            containers: path.resolve(__dirname, 'src/containers')
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'main.css',
+        }),
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            // favicon: './src/imgs/favicon.ico'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: [
+                            [
+                                "@babel/plugin-proposal-class-properties"
+                            ]
+                        ]
+                    }
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                        },
+                    }
+                ]
+            },
+        ]
+    },
+};
